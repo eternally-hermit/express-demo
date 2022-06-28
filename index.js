75022408e3110b140cd94ee9d7703e13e2690b39
H3RMIT <shubhamhalder0@gmail.com>
2022-06-28 20:18:54 +0530

    Code Works

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 
@@ -15,6 +16,10 @@ app.get('/',(req,res) => {
     res.status(200).send('Hello There!')
 })
 
+app.get('/api/courses',(req,res) => {
+    res.status(200).send(JSON.stringify(courses))
+})
+
 app.get('/api/courses/:id',(req,res) => {
     const course = courses.find((c) => c.id === parseInt(req.params.id) );
     if(!course) res.status(404).send('Course Not Found')
@@ -22,6 +27,14 @@ app.get('/api/courses/:id',(req,res) => {
 })
 
 app.post('/api/courses',(req,res) => {
+
+    // Input Validation for post methods
+    const {error} = validateCourse(req.body)
+    if(error){
+        res.status(400).send(result.error.message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -30,6 +43,30 @@ app.post('/api/courses',(req,res) => {
     res.status(200).send(course)
 })
 
+app.put('/api/courses/:id',(req,res) => {
+
+    // Check if the course exists
+    const course = courses.find((c) => c.id === parseInt(req.params.id) );
+    if(!course) res.status(404).send('Course Not Found')
+
+    // Input Validation
+    const {error} = validateCourse(req.body)
+    if(error){
+        res.status(400).send(result.error.message);
+        return;
+    }
+
+    course.name = req.body.name
+    res.status(200).send(course)
+})
+
 app.listen(PORT,()=> {
     console.log(`Listening on port ${PORT}!`);
 })
+
+const validateCourse = (course) => {
+    const schema = Joi.object({
+        name:Joi.string().min(3).required()
+    })
+    return schema.validate(course)
+}
