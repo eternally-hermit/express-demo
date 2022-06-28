c4090f22c84f025e62d3c9866c2ea0ea7e411149
H3RMIT <shubhamhalder0@gmail.com>
2022-06-28 19:38:54 +0530

    Second Commit of the day!

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const app = express()
 
 const PORT = process.env.PORT||4000;
 
+app.use(express.json())
+
+const courses = [
+    {id:1,name:"course1"},
+    {id:2,name:"course2"},
+    {id:3,name:"course3"}
+]
+
 app.get('/',(req,res) => {
     res.status(200).send('Hello There!')
+})
+
+app.get('/api/courses/:id',(req,res) => {
+    const course = courses.find((c) => c.id === parseInt(req.params.id) );
+    if(!course) res.status(404).send('Course Not Found')
+    res.status(200).send(course);
+})
+
+app.post('/api/courses',(req,res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.status(200).send(course)
 })
 
 app.listen(PORT,()=> {
